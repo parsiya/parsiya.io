850b74ba3b823e3160db26e64de12b2427d1ae82
parsiya <parsiya@gmail.com>
2018-04-22 04:23:59 -0400

    Working as intended

---
+++ archetypes/post.md
@@ -0,0 +1,15 @@
+---
+date: {{ .Date }}
+draft: false
+toc: false
+comments: false
+categories:
+- category1
+tags:
+- tag1
+- tag2
+title: "{{ replace .TranslationBaseName "-" " " | title }}"
+wip: false
+snippet: |
+
+---
---
+++ runme.bat
@@ -0,0 +1,9 @@
+rd /q /s public
+hugo
+rd /q /s public\post
+del /s /a .\*thumbs*.db
+del /s /a public\categories\*index*.xml
+del /s /a public\tags\*index*.xml
+python s3cmd sync --acl-public --delete-removed -MP --rr public/ s3://parsiya.io
+python s3cmd --acl-public --no-preserve --cf-invalidate --mime-type="text/css" put public/css/hugo-octopress.css s3://parsiya.net/css/hugo-octopress.css
+rd /q /s public
